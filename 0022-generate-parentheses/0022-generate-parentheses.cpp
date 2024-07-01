88d5b0d6eb20dbd0348565a0e5ab54da2d519b9e
Aditya <126133127+Aditya2600@users.noreply.github.com>
2024-07-01 20:36:58 +0530

    Time: 0 ms (100%), Space: 11.7 MB (91.61%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    void fun(int n, vector<string> &ans, string &temp, int i=0, int j=0){
+        if(i == n && j==n){
+            ans.push_back(temp);
+            return;
+        }
+        if(i+1<=n){
+            temp.push_back('(');
+            fun(n,ans,temp,i+1,j);
+            temp.pop_back();
+        }
+        if(j+1<=n && j+1<=i){
+            temp.push_back(')');
+            fun(n,ans,temp,i,j+1);
+            temp.pop_back();
+        }
+
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string temp;
+        fun(n,ans,temp);
+        return ans;
+    }
+};
