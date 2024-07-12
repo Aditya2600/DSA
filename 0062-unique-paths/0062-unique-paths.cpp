41912d732a9068390fea3c9ec30452212509ea65
Aditya <126133127+Aditya2600@users.noreply.github.com>
2024-07-12 19:54:58 +0530

    Time: 0 ms (100%), Space: 8.1 MB (17.58%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    int uniquePathsHelper(int i, int j, int m, int n, vector<vector<int>> &dp) {
-        if (i >= n || j >= m) return 0; // Out of bounds
-        if (i == n-1 && j == m-1) return 1; // Reached the destination
-        if (dp[i][j] != -1) return dp[i][j]; // Memoized result
-        dp[i][j] = uniquePathsHelper(i + 1, j, m, n, dp) + uniquePathsHelper(i, j + 1, m, n, dp); // Calculate and memoize
-        return dp[i][j];
-    }
-
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(n, vector<int>(m, -1)); // DP table initialized with -1
-        return uniquePathsHelper(0, 0, m, n, dp); // Start from the top-left corner
+        vector<vector<int>> grid(m,vector<int>(n,0));
+        for(int i=0; i<m; i++) grid[i][0] = 1;
+        for(int j=0; j<n; j++) grid[0][j] = 1;
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                grid[i][j] = grid[i-1][j] + grid[i][j-1];
+            }
+        }
+        return grid[m-1][n-1];
     }
 };
