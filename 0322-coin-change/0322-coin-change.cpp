c3cfe129cfe4959162afec125e984a54c23323f6
Aditya <126133127+Aditya2600@users.noreply.github.com>
2024-07-14 09:28:06 +0530

    Time: 16 ms (94.77%), Space: 16.4 MB (64.27%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
-    int helper(vector<int>& coins, int amount, vector<int> &dp){
+    /*int helper(vector<int>& coins, int amount, vector<int> &dp){
         
         if(amount == 0) return 0;
         if(amount < 0) return -1;
         if(dp[amount] != -2) return dp[amount];
         int ans = INT_MAX; 
         for(int i=0; i<coins.size(); i++){
-            
             int res = min(helper(coins,amount - coins[i],dp),ans);
             if(res >= 0 && res < ans){
                 ans = 1 + res;
@@ -15,9 +14,19 @@ public:
         }
         dp[amount] = (ans == INT_MAX) ? -1 : ans;
         return dp[amount];
-    }
+    }*/
+
+
     int coinChange(vector<int>& coins, int amount) {
-         vector<int> dp(amount + 1,-2);
-         return helper(coins,amount,dp);
+         vector<int> dp(amount + 1,amount + 1);
+         dp[0] = 0;
+         for(int i=1; i<= amount; i++){
+            for(auto coin : coins){
+                if(i - coin >= 0){
+                    dp[i] = min(dp[i],dp[i-coin] + 1);
+                }
+            }
+         }
+         return dp[amount] > amount ? -1 : dp[amount];
     }
 };
