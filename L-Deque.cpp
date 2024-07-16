ae1494d7856b7d24eb09c15fed1fdef581114150
Aditya <126133127+Aditya2600@users.noreply.github.com>
2024-07-16 21:20:03 +0530

    Add files via upload

--- L-Deque.cpp
+++ L-Deque.cpp
@@ -9,15 +9,28 @@ ll arr[3005];
 
 ll f(int i, int j){
     if(i == j) return arr[i];
-    if(dp[i][j] != -1) return dp[i][j];
+    if(dp[i][i] != -1) return dp[i][j];
     return dp[i][j] = max(arr[i] - f(i+1,j), arr[j] - f(i,j-1));
+}
+ll fbu(int n){
+    memset(dp,0,sizeof dp);
+    for(int i=0; i<n; i++){
+    dp[i][i] = arr[i];
+    }
+    for(int len = 2; len <= n; len++){
+        for(int i=0; i<n-len+1; i++){
+            int j = i + len -1;
+            dp[i][j] = max(arr[i] - dp[i+1][j], arr[j] - dp[i][j-1]);
+        }
+    }
+    return dp[0][n-1];
 }
 int main(){
     int n;
     cin>>n;
     for(int i=0; i<n; i++) cin>>arr[i];
-    memset(dp, -1, sizeof(dp));
-    cout<<f(0,n-1)<<"\n";
+    //memset(dp, -1, sizeof(dp));
+    cout<<fbu(n)<<"\n";
     return 0;
 }
      
