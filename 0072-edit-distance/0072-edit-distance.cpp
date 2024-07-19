ab2a4a52de6c3c2c9c2900e62fc0ae5cdb646149
Aditya <126133127+Aditya2600@users.noreply.github.com>
2024-07-20 02:33:39 +0530

    Time: 51 ms (14.8%), Space: 10.5 MB (82.32%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -9,8 +9,23 @@ public:
         return dp[i][j] = min({1 + f(s1,s2,i+1,j+1),1 + f(s1,s2,i+1,j), 1 + f(s1,s2,i,j+1)});
     }
     int minDistance(string word1, string word2) {
-        memset(dp, -1, sizeof dp);
-        return f(word1, word2, 0, 0);
-       
+        memset(dp, 0, sizeof dp);
+        for(int j = 0; j<word2.size(); j++){
+        int i = word1.size();
+        dp[i][j] = word2.size()-j;
+        }
+        for(int i = 0; i<word1.size(); i++){
+            int j = word2.size();
+            dp[i][j] = word1.size() - i;
+        }
+        for(int i = word1.size() - 1; i>=0; i--){
+            for(int j = word2.size() - 1; j>=0; j--){
+                if(word1[i] == word2[j]) dp[i][j] = dp[i+1][j+1];
+            else{
+                dp[i][j] = min({1+dp[i+1][j+1], 1+dp[i+1][j], 1+dp[i][j+1]});
+            }
+        }
+        }
+        return dp[0][0];
     }
 };
