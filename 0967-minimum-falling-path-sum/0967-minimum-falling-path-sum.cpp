78b74416720a1b5c3f7370fbb7740a096d1cf8a5
Aditya <126133127+Aditya2600@users.noreply.github.com>
2024-08-07 06:16:49 +0530

    Time: 11 ms (71.12%), Space: 13.5 MB (75.4%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
-    int minFallingPathSumHelper(vector<vector<int>>& matrix, int r, int col){
-        int n = matrix.size();
-        if(r >= n) return 0;
-        if(col < 0 || col >= n) return 1e8;
-        int leftd = matrix[r][col] + minFallingPathSumHelper(matrix, r+1, col-1);
-        int below = matrix[r][col] + minFallingPathSumHelper(matrix, r+1, col);
-        int rightd = matrix[r][col] + minFallingPathSumHelper(matrix, r+1, col+1);
-        return min(leftd ,min(below, rightd));
-    }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int mini = 1e8;
-        vector<vector<int>> dp(n ,vector<int> (n,0));
+        vector<int> prev(n,0), curr(n,0);
         for(int j=0; j<n; j++){
-            dp[0][j] = matrix[0][j];
+            prev[j] = matrix[0][j];
         }
         for(int i=1; i<n; i++){
             for(int j=0; j<n; j++){
                 int leftd = matrix[i][j];
-                if(j-1>=0) leftd += dp[i-1][j-1];
+                if(j-1>=0) leftd += prev[j-1];
                 else leftd += 1e8;
-                int below = matrix[i][j] + dp[i-1][j];
+                int below = matrix[i][j] + prev[j];
                 int rightd = matrix[i][j]; 
-                if(j+1 < n) rightd += dp[i-1][j+1];
+                if(j+1 < n) rightd += prev[j+1];
                 else rightd += 1e8;
-                dp[i][j] = min(leftd ,min(below, rightd));
+                curr[j] = min(leftd ,min(below, rightd));
             }
+            prev = curr;
         }
         
         for(int j=0; j<n; j++){
-            mini = min(mini, dp[n-1][j]);
+            mini = min(mini, prev[j]);
         }
         return mini;
     }
