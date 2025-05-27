6642f7e0e8cb45428a99bc7e63d68ce3ab94af89
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-05-27 20:04:33 +0530

    Time: 154 ms (13.85%), Space: 61.7 MB (35.12%) - LeetHub

--- 3860-resulting-string-after-adjacent-removals/3860-resulting-string-after-adjacent-removals.cpp
+++ 3860-resulting-string-after-adjacent-removals/3860-resulting-string-after-adjacent-removals.cpp
@@ -2,45 +2,20 @@ class Solution {
 public:
     string resultingString(string s) {
         stack<char> st;
-
-    for (char c : s) {
-        if (!st.empty()) {
-            char top = st.top();
-            int diff = abs(top - c);
-            // Check if consecutive (including circular)
-            if (diff == 1 || diff == 25) {
-                st.pop(); // remove the pair
-                continue;
+        for(int i=0; i<s.size(); i++){
+            if(st.size() && (abs(st.top() - s[i]) == 1 || abs(st.top() - s[i]) == 25)){
+                st.pop();
+            }
+            else{
+                st.push(s[i]);
             }
         }
-        st.push(c);
-    }
-
-    // Build final result from stack
-    string result(st.size(), ' ');
-    for (int i = st.size() - 1; i >= 0; --i) {
-        result[i] = st.top();
-        st.pop();
-    }
-
-    return result;
-
-        // bool changed = true;
-
-        // while (changed) {
-        //     changed = false;
-        //     for (int i = 0; i < s.size() - 1; ++i) {
-        //         int j = i + 1;
-        //         char a = s[i];
-        //         char b = s[j];
-        //         if (abs(a - b) == 1 || (a == 'a' && b == 'z') || (a == 'z' && b == 'a')) {
-        //             s.erase(i, 2);
-        //             changed = true;
-        //             break;
-        //         }
-        //     }
-        // }
-
-        // return s;
+        string ans;
+        while(st.size()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
