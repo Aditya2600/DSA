636ed970f4c51650f46ddf0d3f339c30d66d7a88
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-06-01 16:18:03 +0530

    Time: 0 ms (100%), Space: 7.7 MB (96.55%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> grid(m,vector<int>(n,0));
-        for(int i=0; i<m; i++) grid[i][0] = 1;
-        for(int j=0; j<n; j++) grid[0][j] = 1;
-        for(int i=1; i<m; i++){
-            for(int j=1; j<n; j++){
-                grid[i][j] = grid[i-1][j] + grid[i][j-1];
-            }
+    long long ncr(int n, int r) {
+        long long ans = 1;
+        for (int i = 1; i <= min(r, n - r); i++) {
+            ans = (ans * (n - i + 1)) / i;
         }
-        return grid[m-1][n-1];
+        return ans;
     }
+    int uniquePaths(int m, int n) { return ncr(m + n - 2, min(m - 1, n - 1)); }
 };
