b719ed9a500c278057bb7f415470b7a8ec98ea55
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-06-04 17:25:43 +0530

    Time: 3 ms (65.74%), Space: 12.4 MB (19.69%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     vector<vector<char>> grid;
     vector<vector<string>> result;
-    bool canPlaceQueen(int row, int col, int n){
-        for(int i=row-1; i>=0; i--){
-            if(grid[i][col] == 'Q'){
-                return false;
-            }
-        }
-        for(int i=row-1, j=col-1; i>=0 and j>=0; i--, j--){
-            if(grid[i][j] == 'Q'){
-                return false;
-            }
-        }
-        for(int i=row-1, j=col+1; i>=0 and j<n; i--, j++){
-            if(grid[i][j] == 'Q'){
-                return false;
-            }
-        }
-        return true;
-    }
-    void f(int row, int n){
+    // bool canPlaceQueen(int row, int col, int n){
+    //     for(int i=row-1; i>=0; i--){
+    //         if(grid[i][col] == 'Q'){
+    //             return false;
+    //         }
+    //     }
+    //     for(int i=row-1, j=col-1; i>=0 and j>=0; i--, j--){
+    //         if(grid[i][j] == 'Q'){
+    //             return false;
+    //         }
+    //     }
+    //     for(int i=row-1, j=col+1; i>=0 and j<n; i--, j++){
+    //         if(grid[i][j] == 'Q'){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+    void f(int row, int n, int colMask, int ld_mask, int rd_mask){
         if(row == n){
             vector<string> temp;
             for(int i=0; i<n; i++){
@@ -33,17 +33,21 @@ public:
             result.push_back(temp);
             return;
         }
+        
         for(int col=0; col<n; col++){
-            if(canPlaceQueen(row, col, n)){
+            int colBit = 1<<col;
+            int ldBit = 1<<(row-col+n-1);
+            int rdBit = 1<<(row+col);
+            if((colMask & colBit) == 0 && (rd_mask & rdBit) == 0 and (ld_mask & ldBit) == 0){
                 grid[row][col] = 'Q';
-                f(row+1, n);
+                f(row+1, n, colMask|colBit, ld_mask|ldBit, rd_mask|rdBit);
                 grid[row][col] = '.';
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         grid.resize(n, vector<char> (n, '.'));
-        f(0, n);
+        f(0, n, 0, 0, 0);
         return result;
     }
 };
