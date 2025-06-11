22fafdee607c1ea32b420cf1d9ca0b96b06ac9a7
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-06-11 10:54:36 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.64%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
-    vector<int> dp;
+    int dp[20];
     int f(int n){
         if(n == 0 || n == 1) return 1;
         if(n == 2) return 2;
         if(dp[n] != -1) return dp[n];
         int sum = 0;
         for(int k=1; k<=n; k++){
-            sum += f(k-1)*f(n-k);
+            sum = sum + f(k-1)*f(n-k);
         }
-        return dp[n] = sum;
+        return dp[n] =  sum;
     }
     int numTrees(int n) {
-        dp.clear();
-        dp.resize(30,0);
-        dp[0] = dp[1] = 1;
-        dp[2] = 2;
-        for(int i = 3; i<=n; i++){
-            for(int k = 1; k <= i; k++){
-                dp[i] += dp[k-1]*dp[i-k];
-            }
-        }
-        return dp[n];
+        memset(dp, -1, sizeof dp);
+        return f(n);
     }
 };
