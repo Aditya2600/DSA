faee840ca32e014e4dc3aa377aa3e4687e715533
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-06-19 19:18:05 +0530

    Time: 2043 ms (5.02%), Space: 9.5 MB (57.04%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
-    bool isPalindrome(int i, int j, string& s) {
-        while (i < j) {
-            if (s[i] != s[j])
+    bool isPalindrome(string& s, int i, int j){
+        while(i <= j){
+            if(s[i++] != s[j--]){
                 return false;
-            i++;
-            j--;
+            }
         }
         return true;
     }
-    // int f(int i, int n, string &s,vector<int> &dp){
-    //     if(i == n) return 0;
-    //     int minCost = INT_MAX;
-    //     if(dp[i] != -1) return dp[i];
-    //     for(int j=i; j<n; j++){
-    //         if(isPalindrome(i, j, s)){
-    //             int cost = 1 + f(j+1, n, s, dp);
-    //             minCost = min(minCost, cost);
-    //         }
-    //     }
-    //     return dp[i] = minCost;
-    // }
+    int f(int i, string &s, vector<int>& dp, int n){
+        if(i == s.size()){
+            return 0;
+        }
+        int minCost = INT_MAX;
+        if(dp[i] != -1) return dp[i];
+        for(int j=i; j<s.size(); j++){
+            if(isPalindrome(s, i, j)){
+                int cost = 1 + f(j+1, s, dp, n);
+                minCost = min(minCost, cost);
+            }
+        }
+        
+        return dp[i] = minCost;
+    }
     int minCut(string s) {
         int n = s.size();
-        vector<int> dp(n+1, 0);
-        for (int i = n - 1; i >= 0; i--) {
-            int minCost = INT_MAX;
-            for (int j = i; j < n; j++) {
-                if (isPalindrome(i, j, s)) {
-                    int cost = 1 + dp[j + 1];
-                    minCost = min(minCost, cost);
-                }
-            }
-            dp[i] = minCost;
+        vector<int> dp(n, -1);
+        return f(0, s, dp, n) - 1;
     }
-    return dp[0] - 1;
-}
-}
-;
+};
