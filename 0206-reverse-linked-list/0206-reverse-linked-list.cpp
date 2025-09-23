4ba2bd576f3ab1a102ddea9210794284c8df0ce9
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-09-23 20:35:07 +0530

    Time: 1 ms (2.5%), Space: 13.4 MB (39.65%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -8,25 +8,39 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         ListNode* temp = head;
+//         ListNode* prev = nullptr;
+//         while(temp != nullptr){
+//             head = temp -> next;
+//             temp -> next = prev;
+//             prev = temp;
+//             temp = head;
+//         }
+//         return prev;
+//     }
+// };
+// struct ListNode {
+//     int val;
+//     ListNode* next;
+//     ListNode() : val(0), next(nullptr){}
+//     ListNode(int x) : val(x), next(nullptr){}
+//     ListNode(int x, ListNode* next) : val(x), next(next){}
+
+// }
+
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* temp = head;
-        ListNode* prev = nullptr;
-        while(temp != nullptr){
-            head = temp -> next;
-            temp -> next = prev;
-            prev = temp;
-            temp = head;
+        if(head == nullptr || head -> next == nullptr){
+            return head;
         }
-        return prev;
+        ListNode *newHead = reverseList(head -> next);
+        ListNode *front = head -> next;
+        front -> next = head;
+        head -> next = nullptr;
+        return newHead;
     }
 };
-
-// struct ListNode{
-//     int val;
-//     ListNode* next;
-//     ListNode() : val(0), next(nullptr) {}
-//     ListNode(int x) : val(x), next(nullptr) {}
-//     ListNode(int x, ListNode* next) : val(x), next(next) {}
-// }
