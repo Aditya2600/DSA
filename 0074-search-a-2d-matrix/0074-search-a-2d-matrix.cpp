24bd27172ec975e353638fbcc03a6cba82ec6e82
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-09-24 16:30:04 +0530

    Time: 0 ms (100%), Space: 13.2 MB (76.57%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,14 +1,36 @@
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         for(int i=0; i<matrix.size(); i++){
+//             for(int j=matrix[0].size() - 1; j >= 0; j--){
+//                 if(matrix[i][j] == target){
+//                     return true;
+//                 }
+//                 if(matrix[i][j] < target){
+//                     break;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// };
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for(int i=0; i<matrix.size(); i++){
-            for(int j=matrix[0].size() - 1; j >= 0; j--){
-                if(matrix[i][j] == target){
-                    return true;
-                }
-                if(matrix[i][j] < target){
-                    break;
-                }
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int lo = 0, hi = n * m - 1;
+        
+        while(lo <= hi){
+            int mid = lo + (hi - lo)/2;
+            int row = mid / m;
+            int col = mid % m;
+            if(matrix[row][col] == target) return true;
+            else if(matrix[row][col] < target){
+                lo = mid + 1;
+            }
+            else{
+                hi = mid - 1;
             }
         }
         return false;
