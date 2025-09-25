f52950930594dfbc09f46694e3a3e456b2a90513
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-09-25 19:01:03 +0530

    Time: 0 ms (100%), Space: 9.5 MB (8.86%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
-    int ncr(int n, int r) {
-        int ans = 1;
-        for (int i = 1; i <= min(r, n - r); i++) {
-            ans = (ans * (n - i + 1)) / i;
+    int f(int i, int j, int m, int n, vector<vector<int>>& dp) {
+        if (i == m - 1 && j == n - 1) {
+            return 1;
         }
-        return ans;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(i >= m || j >= n) return 0;
+        dp[i][j] = f(i + 1, j, m, n, dp) + f(i, j + 1, m, n, dp);
+        return dp[i][j];
     }
-    int uniquePaths(int m, int n) { return ncr(m + n - 2, min(m - 1, n - 1)); }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m+1, vector<int> (n+1, -1));
+        return f(0, 0, m, n, dp);
+    }
 };
