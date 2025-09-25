7fd52975ba9ce704548e93c4e7f04d99c30cf0fa
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-09-25 22:22:50 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (96.75%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -31,18 +31,33 @@
 //         return dp[0][0];
 //     }
 // };
+// class Solution {
+// public:
+//     int uniquePaths(int m, int n) {
+//         vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+//         for(int i=0; i<m; i++) dp[i][0] = 1;
+//         for(int j=0; j<n; j++) dp[0][j] = 1;
+//         for(int i=1; i<m; i++){
+//             for(int j=1; j<n; j++){
+//                 dp[i][j] = dp[i-1][j] + dp[i][j-1];
+//             }
+//         }
+//         return dp[m-1][n-1];
+//     }
+// };
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-
-        for(int i=0; i<m; i++) dp[i][0] = 1;
-        for(int j=0; j<n; j++) dp[0][j] = 1;
-        for(int i=1; i<m; i++){
-            for(int j=1; j<n; j++){
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
-            }
+    long long ncr(int n, int r){
+        if(n <= 0) return 1;
+        if(r >= n) return 0;
+        long long res = 1;
+        for(int i=1; i<=min(r, n-r); i++){
+            res = res * (n-i+1)/i;
         }
-        return dp[m-1][n-1];
+        return res;
+    }
+    int uniquePaths(int m, int n) {
+        return ncr(m+n-2, min(m-1, n-1));
     }
 };
