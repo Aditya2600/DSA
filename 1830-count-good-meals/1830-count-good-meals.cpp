61c4749b3045c0c328bc44fac41642f300beff0d
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-10-04 23:52:25 +0530

    Time: 215 ms (54.36%), Space: 76 MB (93.96%) - LeetHub

--- 1830-count-good-meals/1830-count-good-meals.cpp
+++ 1830-count-good-meals/1830-count-good-meals.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int countPairs(vector<int>& deliciousness) {
-
-        const int MOD = 1000000007;
-        unordered_map<int,int> cnt;
         long long ans = 0;
-
+        int mod = 1000000007;
+        unordered_map<int, int> mp;
         for (int x : deliciousness) {
-            for (int i = 0; i <= 21; ++i) {
-                int power = 1 << i;
-                int need = power - x;
-                auto it = cnt.find(need);
-                if (it != cnt.end()) ans = (ans + it->second) % MOD;
+            for (int i = 0; i <= 21; i++) {
+                int target = 1 << i;
+                int need = target - x;
+                auto it = mp.find(need);
+                if (it != mp.end()) {
+                    ans += (mp[need] % mod);
+                }
             }
-            cnt[x]++; 
+            mp[x]++;
         }
-        return (int)ans;
-        
+        return (int)(ans % mod);
     }
 };
