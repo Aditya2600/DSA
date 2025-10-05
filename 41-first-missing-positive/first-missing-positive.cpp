107286cba46dc720691a44eff591bbedd87eceb8
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-10-05 21:26:30 +0530

    Time: 3 ms (55.45%) | Memory: 55.1 MB (49.39%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -18,24 +18,43 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int firstMissingPositive(vector<int>& nums) {
+//         int n = nums.size();
+
+//         for (int i = 0; i < n; i++) {
+//             if (nums[i] <= 0) {
+//                 nums[i] = n + 1;
+//             }
+//         }
+//         for(int i=0; i<n; i++){
+//             if(abs(nums[i]) <= n && nums[abs(nums[i]) - 1] > 0){
+//                 nums[abs(nums[i]) - 1] *= -1;
+//             }
+//         }
+//         for (int i = 0; i < n; i++) {
+//             if (nums[i] > 0) {
+//                 return i + 1;
+//             }
+//         }
+//         return n+1;
+//     }
+// };
+
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         int n = nums.size();
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] <= 0) {
-                nums[i] = n + 1;
-            }
-        }
         for(int i=0; i<n; i++){
-            if(abs(nums[i]) <= n && nums[abs(nums[i]) - 1] > 0){
-                nums[abs(nums[i]) - 1] *= -1;
+            while(nums[i] <= n && nums[i] >= 1 && nums[i] != nums[nums[i] - 1]){
+                swap(nums[i], nums[nums[i] - 1]);
             }
         }
         for (int i = 0; i < n; i++) {
-            if (nums[i] > 0) {
-                return i + 1;
+            if (nums[i] != i+1) {
+                return i+1;
             }
         }
         return n+1;
