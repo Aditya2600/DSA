870e8dababced627300418debc63dc8f06e4a20f
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-10-05 18:04:21 +0530

    Time: 66 ms (37.54%), Space: 47.8 MB (26.77%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,16 +1,38 @@
+// class Solution {
+// public:
+//     int subarraySum(vector<int>& nums, int k) {
+//         int cnt = 0;
+//         int sum = 0;
+//         int l = 0;
+
+//         for (int r = 0; r < nums.size(); r++) {
+//             sum += nums[r];
+//             while (l <= r && sum > k) {
+//                 sum -= nums[l++];
+//             }
+//             while (l <= r && sum == k) {
+//                 cnt++;
+//                 sum -= nums[l++];
+//             }
+//         }
+//         return cnt;
+//     }
+// };
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
-        unordered_map<int,int> mp;
-        mp[0] = 1;
-        int prefix = 0, cnt = 0;
-        for(int i=0; i<n; i++){
-            prefix += nums[i];
-            int remove = prefix - k;
-            cnt += mp[remove];
-            mp[prefix]++;
-        }
-        return cnt;
+       map<int, int> mp;
+       int preSum = 0;
+       mp[0] = 1;
+       int cnt = 0;
+       for(int i=0; i<nums.size(); i++){
+            preSum += nums[i];
+            int exclude = preSum - k;
+            if(mp.find(exclude) != mp.end()){
+                cnt += mp[exclude];
+            }
+            mp[preSum] += 1;
+       }
+       return cnt;
     }
 };
