7fcbc371a4a0cf6a6688a4835fb6412b353f4e51
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-10-07 14:14:34 +0530

    Time: 0 ms (100%), Space: 9.9 MB (60.13%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,19 +1,41 @@
+// class Solution {
+// public:
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         int n = nums.size();
+//         int m = 1 << n;
+//         vector<vector<int>> powerSet;
+
+//         for (int i = 0; i < m; i++) {
+//             vector<int> set;
+//             for (int j = 0; j < n; j++) {
+//                 if (i & (1 << j)) {
+//                     set.push_back(nums[j]);
+//                 }
+//             }
+//             powerSet.push_back(set);
+//         }
+//         return powerSet;
+//     }
+// };
 class Solution {
 public:
+
+    void solve(vector<int>& nums, int ind, vector<vector<int>> &ans, vector<int> &set){
+        int n = nums.size();
+        if(ind >= n){
+            ans.push_back(set);
+            return;
+        }
+        set.push_back(nums[ind]);
+        solve(nums, ind+1, ans, set);
+        set.pop_back();
+        solve(nums, ind+1, ans, set);
+    }
     vector<vector<int>> subsets(vector<int>& nums) {
         int n = nums.size();
-        int m = 1 << n;
-        vector<vector<int>> powerSet;
-
-        for (int i = 0; i < m; i++) {
-            vector<int> set;
-            for (int j = 0; j < n; j++) {
-                if (i & (1 << j)) {
-                    set.push_back(nums[j]);
-                }
-            }
-            powerSet.push_back(set);
-        }
-        return powerSet;
+        vector<vector<int>> ans;
+        vector<int> set;
+        solve(nums, 0, ans, set);
+        return ans;
     }
 };
