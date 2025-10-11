592007eca8e030541e4be275aa8247f88c5128f3
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-10-11 15:27:29 +0530

    Time: 55 ms (19.65%), Space: 48.7 MB (45.62%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-
-    int f(int ind, vector<int>& coins, int amount, vector<vector<int>> &dp){
-        if(amount == 0){
-            return 0;
-        }
-        if(ind == -1){
-            return 1e9;
-        }
-
-        if(dp[ind][amount] != -1){
-            return dp[ind][amount];
-        }    
-        int take = 1e9;
-        if(coins[ind] <= amount) take = 1 + f(ind, coins, amount - coins[ind], dp);
-        int notTake = f(ind-1, coins, amount, dp);
-        return dp[ind][amount] = min(take, notTake);
-    }
-
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int> (amount+1, -1));
-        int ans = f(n-1, coins, amount, dp);
-        return (ans == 1e9) ? -1 : ans;
+        vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
+       for (int t = 0; t <= amount; t++) {
+            if (t % coins[0] == 0) {
+                dp[0][t] = t/coins[0];
+            } else
+                dp[0][t] = 1e9;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int t = 0; t <= amount; t++) {
+                int take = 1e9;
+                if (coins[i] <= t)
+                    take = 1 + dp[i][t - coins[i]];
+                int notTake = dp[i - 1][t];
+                dp[i][t] = min(take, notTake);
+            }
+        }
+        int ans = dp[n-1][amount];
+        if(ans >= 1e9) return -1;
+        return ans;
     }
 };
