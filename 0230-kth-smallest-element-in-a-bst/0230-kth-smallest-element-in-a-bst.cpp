a586c132d8ac7a58af7e6051b9f6e0e32f0f01fd
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-10-12 13:04:32 +0530

    Time: 0 ms (100%), Space: 24.3 MB (89.97%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -6,22 +6,40 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    // Helper function to perform in-order traversal
-    void inorder(TreeNode* root, vector<int>& elements) {
-        if (!root) return;
-        inorder(root->left, elements);  // Traverse left subtree
-        elements.push_back(root->val);  // Visit current node
-        inorder(root->right, elements); // Traverse right subtree
-    }
-    
+
     int kthSmallest(TreeNode* root, int k) {
-        vector<int> elements;
-        inorder(root, elements); // Perform in-order traversal to get sorted elements
-        return elements[k - 1];  // Return the k-th smallest element (1-based index)
+        TreeNode* curr = root;
+        int cnt = 0;
+        int ans = -1;
+        while (curr != NULL) {
+            if (curr->left == NULL) {
+                cnt++;
+                if(cnt == k) ans = curr->val;
+                curr = curr->right;
+            } else {
+                TreeNode* prev = curr;
+                prev = prev->left;               
+                while (prev->right && prev->right != curr){
+                    prev = prev->right;
+                }
+                if(prev -> right == NULL){
+                    prev->right = curr;
+                    curr = curr->left;
+                }
+                else{
+                    prev -> right = NULL;
+                    cnt++;
+                    if(cnt == k) ans = curr->val;
+                    curr = curr->right;
+                }
+            }
+        }
+        return ans;
     }
 };
