4e11021dc610b10fd531db67540e07c717c67de6
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-10-13 17:52:04 +0530

    Time: 33 ms (83.18%), Space: 18.4 MB (82.5%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -14,8 +14,8 @@
 //         ListNode* a = headA;
 //         ListNode* b = headB;
 
-//         // After at most 2 passes, either meet at intersection or both hit nullptr
-//         while (a != b) {
+//         // After at most 2 passes, either meet at intersection or both hit
+//         nullptr while (a != b) {
 //             a = a ? a->next : headB;
 //             b = b ? b->next : headA;
 //         }
@@ -23,22 +23,59 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+//         ListNode* temp = headA;
+//         unordered_map<ListNode*, int> mp;
+//         while(temp != NULL){
+//             mp[temp] = 1;
+//             temp = temp -> next;
+//         }
+//         temp = headB;
+//         while(temp != NULL){
+//             if(mp.find(temp) != mp.end()){
+//                 return temp;
+//             }
+//             temp = temp -> next;
+//         }
+//         return NULL;
+//     }
+// };
+
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* temp = headA;
-        unordered_map<ListNode*, int> mp;
-        while(temp != NULL){
-            mp[temp] = 1;
-            temp = temp -> next;
+
+    ListNode* collisionPoint(ListNode* headA, ListNode* headB, int len){
+        ListNode* hb = headB;
+        while(len--){
+            hb = hb -> next;
         }
-        temp = headB;
-        while(temp != NULL){
-            if(mp.find(temp) != mp.end()){
-                return temp;
-            }
-            temp = temp -> next;
+        ListNode* ha = headA;
+        while(ha != hb){
+            ha = ha -> next;
+            hb = hb -> next;
         }
-        return NULL;
+        return ha;
+    }
+
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+        int lenA = 0;
+        int lenB = 0;
+        ListNode* h1 = headA;
+        while (h1 != NULL) {
+            lenA++;
+            h1 = h1->next;
+        }
+        
+        ListNode* h2 = headB;
+        while(h2 != NULL){
+            lenB++;
+            h2 = h2 -> next;
+        }
+        if(lenA < lenB){
+            return collisionPoint(headA, headB, lenB - lenA);
+        }
+        else return collisionPoint(headB, headA, lenA - lenB);
     }
 };
