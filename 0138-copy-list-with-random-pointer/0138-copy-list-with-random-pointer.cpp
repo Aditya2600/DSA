f8ffcfa13dbed4b5b4756e4220f363d9c5902159
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-10-14 15:26:16 +0530

    Time: 6 ms (55.62%), Space: 15.1 MB (69.71%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -5,7 +5,7 @@ public:
     int val;
     Node* next;
     Node* random;
-    
+
     Node(int _val) {
         val = _val;
         next = NULL;
@@ -14,25 +14,62 @@ public:
 };
 */
 
+// class Solution {
+// public:
+//     Node* copyRandomList(Node* head) {
+//         Node* temp = head;
+//         unordered_map<Node*, Node*> mp;
+//         while(temp != NULL){
+//             Node* newNode = new Node(temp -> val);
+//             mp[temp] = newNode;
+//             temp = temp -> next;
+//         }
+//         temp = head;
+
+//         while(temp != NULL){
+//             Node* copyNode = mp[temp];
+//             copyNode -> next = mp[temp -> next];
+//             copyNode -> random = mp[temp -> random];
+//             copyNode = copyNode -> next;
+//             temp = temp -> next;
+//         }
+//         return mp[head];
+//     }
+// };
+
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
         Node* temp = head;
-        unordered_map<Node*, Node*> mp;
-        while(temp != NULL){
-            Node* newNode = new Node(temp -> val);
-            mp[temp] = newNode;
-            temp = temp -> next;
+        while (temp != NULL) {
+            Node* copyNode = new Node(temp -> val);
+            copyNode->next = temp->next;
+            temp->next = copyNode;
+            temp = temp -> next -> next;
         }
-        temp = head;
 
+        temp = head;
+        while (temp != NULL) {
+            Node* copyNode = temp -> next;
+            if(temp -> random != NULL){
+                copyNode -> random = temp -> random -> next;
+            }
+            else{
+                copyNode -> random = NULL;
+            }
+            temp = copyNode -> next;
+        }
+
+        Node* dummy = new Node(-1);
+        Node* res = dummy;
+        temp = head;
         while(temp != NULL){
-            Node* copyNode = mp[temp];
-            copyNode -> next = mp[temp -> next];
-            copyNode -> random = mp[temp -> random];
-            copyNode = copyNode -> next;
+            Node* copyNode = temp -> next;
+            res -> next = copyNode;
+            res = res -> next;
+            temp -> next = copyNode -> next;
             temp = temp -> next;
         }
-        return mp[head];
+        return dummy -> next;
     }
 };
