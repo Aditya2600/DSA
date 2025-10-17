d0a886c33013f1ca392fcbd9da22b3a79368b180
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-10-17 21:37:56 +0530

    Time: 1515 ms (33.70%) | Memory: 9.2 MB (84.91%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -10,25 +10,38 @@ public:
         }
         return true;
     }
-    int f(int ind, string& s, vector<int>& dp) {
-        int n = s.size();
-        if (ind == n) {
-            return 0;
-        }
-        if(dp[ind] != -1) return dp[ind];
-        int ans = 1e9;
-        for (int i = ind; i < n; i++) {           
-            if (isPalindrome(s, ind, i)) {
-                int cut = (i == n-1) ? 0 : 1 + f(i + 1, s, dp);
-                ans = min(ans, cut);
-            }
-        }
-        
-        return dp[ind] = ans;
-    }
+    // int f(int ind, string& s, vector<int>& dp) {
+    //     int n = s.size();
+    //     if (ind == n) {
+    //         return 0;
+    //     }
+    //     if(dp[ind] != -1) return dp[ind];
+    //     int ans = 1e9;
+    //     for (int i = ind; i < n; i++) {
+    //         if (isPalindrome(s, ind, i)) {
+    //             int cut = (i == n-1) ? 0 : 1 + f(i + 1, s, dp);
+    //             ans = min(ans, cut);
+    //         }
+    //     }
+
+    //     return dp[ind] = ans;
+    // }
+
     int minCut(string s) {
         int n = s.size();
-        vector<int> dp(n,-1);
-        return f(0, s, dp);
+        vector<int> dp(n+1, 0);
+        dp[n] = 0;
+        for (int ind = n - 1; ind >= 0; ind--) {
+            int ans = 1e9;
+            for (int i = ind; i < n; i++) {
+                if (isPalindrome(s, ind, i)) {
+                    int cut = (i == n - 1) ? 0 : 1 + dp[i + 1];
+                    ans = min(ans, cut);
+                }
+            }
+            dp[ind] = ans;
+        }
+
+        return dp[0];
     }
 };
