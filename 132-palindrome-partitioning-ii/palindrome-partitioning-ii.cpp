1d07bd30a2dab88b1efea2b77b121eedf4a37181
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-10-17 21:30:26 +0530

    Time: 1758 ms (14.89%) | Memory: 9.4 MB (72.93%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,44 +1,34 @@
 class Solution {
 public:
-    bool isPalindrome(string& s, int i, int j){
-        while(i <= j){
-            if(s[i++] != s[j--]){
+    bool isPalindrome(string& s, int l, int r) {
+        while (l < r) {
+            if (s[l] != s[r]) {
                 return false;
             }
+            l++;
+            r--;
         }
         return true;
     }
-    // int f(int i, string &s, vector<int>& dp, int n){
-    //     if(i == s.size()){
-    //         return 0;
-    //     }
-    //     int minCost = INT_MAX;
-    //     if(dp[i] != -1) return dp[i];
-    //     for(int j=i; j<s.size(); j++){
-    //         if(isPalindrome(s, i, j)){
-    //             int cost = 1 + f(j+1, s, dp, n);
-    //             minCost = min(minCost, cost);
-    //         }
-    //     }
+    int f(int ind, string& s, vector<int>& dp) {
+        int n = s.size();
+        if (ind == n) {
+            return 0;
+        }
+        if(dp[ind] != -1) return dp[ind];
+        int ans = 1e9;
+        for (int i = ind; i < n; i++) {           
+            if (isPalindrome(s, ind, i)) {
+                int cut = (i == n-1) ? 0 : 1 + f(i + 1, s, dp);
+                ans = min(ans, cut);
+            }
+        }
         
-    //     return dp[i] = minCost;
-    // }
+        return dp[ind] = ans;
+    }
     int minCut(string s) {
         int n = s.size();
-        vector<int> dp(n+1, 0);
-        dp[n] = 0;
-        for(int i = n-1; i>=0; i--){
-        int minCost = INT_MAX;
-            for(int j=i; j<n; j++){
-                if(isPalindrome(s, i, j)){
-                    int cost = 1 + dp[j+1];
-                    minCost = min(minCost, cost);
-                }
-            }
-            dp[i] = minCost;
-        }
-        return dp[0] - 1;
-        // vector<int> dp(n, -1);
-        // return f(0, s, dp, n) - 1;
+        vector<int> dp(n,-1);
+        return f(0, s, dp);
     }
 };
