971b9aa5043b46c798dc21382fcddd85d868f564
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-10-17 20:27:25 +0530

    Time: 4 ms (59.67%), Space: 26.6 MB (33.62%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    bool isPalindromeRange(const string& s, int l, int r) {
-        while(l < r) {
-            if(s[l] != s[r]) return false;
+    bool isPalindrome(string s, int l, int r){
+        while(l < r){
+            if(s[l] != s[r]){
+                return false;
+            }
             l++;
             r--;
         }
         return true;
     }
-
     bool validPalindrome(string s) {
+        int n = s.size();
         int l = 0;
         int r = s.size() - 1;
-        while(l < r) {
-            if(s[l] != s[r]) {
-                // Try skipping left or right character
-                return isPalindromeRange(s, l+1, r) || isPalindromeRange(s, l, r-1);
+        while(l < r){
+            if(s[l] != s[r]){
+                return (isPalindrome(s, l+1, r) || isPalindrome(s, l, r-1));
             }
             l++;
             r--;
