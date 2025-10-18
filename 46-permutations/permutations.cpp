795bce051734227a451435c867ef0cb6bfef9f91
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-10-18 22:40:55 +0530

    Time: 4 ms (12.50%) | Memory: 11 MB (25.09%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,25 +1,100 @@
+// // class Solution {
+// // public:
+// //     void backTrack(vector<int>& nums, vector<vector<int>>& ans, vector<int>& res, vector<bool> used){
+// //         int n = nums.size();
+// //         if(res.size() == n){
+// //             ans.push_back(res);
+// //             return;
+// //         }
+// //         for(int i=0; i<n; i++){
+// //             if(used[i]) continue;
+// //             used[i] = true;
+// //             res.push_back(nums[i]);
+// //             backTrack(nums, ans, res, used);
+// //             res.pop_back();
+// //             used[i] = false;
+// //         }
+// //     }
+// //     vector<vector<int>> permute(vector<int>& nums) {
+// //         vector<vector<int>> ans;
+// //         vector<int> res;
+// //         vector<bool> used(nums.size(), false);
+// //         backTrack(nums, ans, res, used);
+// //         return ans;
+// //     }
+// // };
+
+// class Solution {
+// public:
+//     void backTrack(vector<int>& nums, vector<vector<int>>& ans, vector<int>& res, vector<bool> used){
+//         int n = nums.size();
+//         if(res.size() == n){
+//             ans.push_back(res);
+//             return;
+//         }
+//         for(int i=0; i<n; i++){
+//             if(used[i]) continue;
+//             used[i] = true;
+//             res.push_back(nums[i]);
+//             backTrack(nums, ans, res, used);
+//             res.pop_back();
+//             used[i] = false;
+//         }
+//     }
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         vector<vector<int>> ans;
+//         backTrack(0 ,nums, ans);
+//         return ans;
+//     }
+// };
+
+// class Solution {
+// public:
+//     void backTrack(vector<int>& nums, vector<vector<int>>& ans, vector<int>& res, vector<bool> used){
+//         int n = nums.size();
+//         if(res.size() == n){
+//             ans.push_back(res);
+//             return;
+//         }
+//         for(int i=0; i<n; i++){
+//             if(used[i]) continue;
+//             used[i] = true;
+//             res.push_back(nums[i]);
+//             backTrack(nums, ans, res, used);
+//             res.pop_back();
+//             used[i] = false;
+//         }
+//     }
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         vector<vector<int>> ans;
+//         vector<int> res;
+//         vector<bool> used(nums.size(), false);
+//         backTrack(nums, ans, res, used);
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-    void backTrack(vector<int>& nums, vector<vector<int>>& ans, vector<int>& res, vector<bool> used){
+    void solve(int ind, vector<int>& nums, vector<vector<int>>& ans){
         int n = nums.size();
-        if(res.size() == n){
-            ans.push_back(res);
+        if(ind == n){
+            vector<int> ds;
+            for(int i=0; i<n; i++){
+                ds.push_back(nums[i]);
+            }
+            ans.push_back(ds);
             return;
         }
-        for(int i=0; i<n; i++){
-            if(used[i]) continue;
-            used[i] = true;
-            res.push_back(nums[i]);
-            backTrack(nums, ans, res, used);
-            res.pop_back();
-            used[i] = false;
+        for(int i=ind; i<n; i++){
+            swap(nums[ind], nums[i]);
+            solve(ind+1, nums, ans);
+            swap(nums[i], nums[ind]);           
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> res;
-        vector<bool> used(nums.size(), false);
-        backTrack(nums, ans, res, used);
+        solve(0 ,nums, ans);
         return ans;
     }
 };
