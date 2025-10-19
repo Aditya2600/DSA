4c7dc861f7368e40dab1f90676a1388aa52f5e3f
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-10-19 15:10:16 +0530

    Time: 48 ms (60.53%) | Memory: 48 MB (71.65%) - LeetSync

--- 1120-flower-planting-with-no-adjacent/flower-planting-with-no-adjacent.cpp
+++ 1120-flower-planting-with-no-adjacent/flower-planting-with-no-adjacent.cpp
@@ -1,3 +1,39 @@
+// class Solution {
+// public:
+//     bool isSafe(int v, vector<int>& colors, int col, vector<vector<int>>&
+//     adj) {
+//         for (auto e : adj[v]) {
+//             if (colors[e] == col) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+//     bool dfs(int v, vector<vector<int>>& adj, vector<int>& colors) {
+//         if (v == adj.size()) {
+//             return true;
+//         }
+//         for (int c = 1; c <= 4; c++) {
+//             if (isSafe(v, colors, c, adj)) {
+//                 colors[v] = c;
+//                 if(dfs(v + 1, adj, colors)) return true;
+//                 colors[v] = 0;
+//             }
+//         }
+//         return false;
+//     }
+//     vector<int> gardenNoAdj(int n, vector<vector<int>>& paths) {
+//         vector<vector<int>> adj(n);
+//         for (auto e : paths) {
+//             adj[e[0] - 1].push_back(e[1] - 1);
+//             adj[e[1] - 1].push_back(e[0] - 1);
+//         }
+//         vector<int> colors(n, 0);
+//         dfs(0, adj, colors);
+//         return colors;
+//     }
+// };
+
 class Solution {
 public:
     bool isSafe(int v, vector<int>& colors, int col, vector<vector<int>>& adj) {
@@ -8,19 +44,7 @@ public:
         }
         return true;
     }
-    bool dfs(int v, vector<vector<int>>& adj, vector<int>& colors) {
-        if (v == adj.size()) {
-            return true;
-        }
-        for (int c = 1; c <= 4; c++) {
-            if (isSafe(v, colors, c, adj)) {
-                colors[v] = c;
-                if(dfs(v + 1, adj, colors)) return true;
-                colors[v] = 0;
-            }
-        }
-        return false;
-    }
+
     vector<int> gardenNoAdj(int n, vector<vector<int>>& paths) {
         vector<vector<int>> adj(n);
         for (auto e : paths) {
@@ -28,7 +52,13 @@ public:
             adj[e[1] - 1].push_back(e[0] - 1);
         }
         vector<int> colors(n, 0);
-        dfs(0, adj, colors);
+        for (int v = 0; v < n; v++) {
+            for (int c = 1; c <= 4; c++) {
+                if (isSafe(v, colors, c, adj)) {
+                    colors[v] = c;
+                }
+            }
+        }
         return colors;
     }
 };
