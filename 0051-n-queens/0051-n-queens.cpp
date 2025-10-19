9871e6942bc136f20b30ebdefc08576cc77017a5
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-10-19 08:14:57 +0530

    Time: 3 ms (65.33%), Space: 12.5 MB (16.42%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,53 +1,49 @@
 class Solution {
 public:
-    vector<vector<char>> grid;
+    vector<vector<char>> board;
     vector<vector<string>> result;
-    // bool canPlaceQueen(int row, int col, int n){
-    //     for(int i=row-1; i>=0; i--){
-    //         if(grid[i][col] == 'Q'){
-    //             return false;
-    //         }
-    //     }
-    //     for(int i=row-1, j=col-1; i>=0 and j>=0; i--, j--){
-    //         if(grid[i][j] == 'Q'){
-    //             return false;
-    //         }
-    //     }
-    //     for(int i=row-1, j=col+1; i>=0 and j<n; i--, j++){
-    //         if(grid[i][j] == 'Q'){
-    //             return false;
-    //         }
-    //     }
-    //     return true;
-    // }
-    void f(int row, int n, int colMask, int ld_mask, int rd_mask){
-        if(row == n){
+    bool canPlaced(int r, int c, int n) {
+        for (int i = r - 1; i >= 0; i--) {
+            if (board[i][c] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = r - 1, j = c - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = r - 1, j = c + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void backTrack(int r, int n) {
+        if(r == n){
             vector<string> temp;
             for(int i=0; i<n; i++){
                 string res = "";
                 for(int j=0; j<n; j++){
-                    res += grid[i][j];
+                    res += board[i][j];
                 }
                 temp.push_back(res);
             }
             result.push_back(temp);
-            return;
         }
-        
-        for(int col=0; col<n; col++){
-            int colBit = 1<<col;
-            int ldBit = 1<<(row-col+n-1);
-            int rdBit = 1<<(row+col);
-            if((colMask & colBit) == 0 && (rd_mask & rdBit) == 0 and (ld_mask & ldBit) == 0){
-                grid[row][col] = 'Q';
-                f(row+1, n, colMask|colBit, ld_mask|ldBit, rd_mask|rdBit);
-                grid[row][col] = '.';
+        for (int c = 0; c < n; c++) {
+            if (canPlaced(r, c, n)) {
+                board[r][c] = 'Q';
+                backTrack(r + 1, n);
+                board[r][c] = '.';
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        grid.resize(n, vector<char> (n, '.'));
-        f(0, n, 0, 0, 0);
+        board.resize(n, vector<char> (n, '.'));
+        backTrack(0, n);
         return result;
     }
 };
