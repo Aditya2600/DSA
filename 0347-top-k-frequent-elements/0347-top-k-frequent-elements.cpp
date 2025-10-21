b585952a4d6cd1b0c9bb07e77aa028ad8a485c42
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-10-21 23:21:55 +0530

    Time: 4 ms (33.66%), Space: 19.8 MB (13.28%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,3 +1,34 @@
+// class Solution {
+// public:
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         unordered_map<int, int> freq;
+//         for (auto it : nums) {
+//             freq[it]++;
+//         }
+//         sort(nums.begin(), nums.end());
+//         priority_queue<pair<int, int>, vector<pair<int, int>>,
+//                        greater<pair<int, int>>>
+//             pq;
+//         pq.push({freq[nums[0]], nums[0]});
+//         for (int i = 1; i < n; i++) {
+//             if (nums[i] != nums[i-1]) {
+//                 pq.push({freq[nums[i]], nums[i]});
+//                 if (pq.size() > k) {
+//                     pq.pop();
+//                 }
+//             }
+//         }
+//         vector<int> ans;
+//         while (!pq.empty()) {
+//             auto x = pq.top();
+//             pq.pop();
+//             ans.push_back(x.second);
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
@@ -6,25 +37,21 @@ public:
         for (auto it : nums) {
             freq[it]++;
         }
-        sort(nums.begin(), nums.end());
-        priority_queue<pair<int, int>, vector<pair<int, int>>,
-                       greater<pair<int, int>>>
-            pq;
-        pq.push({freq[nums[0]], nums[0]});
-        for (int i = 1; i < n; i++) {
-            if (nums[i] != nums[i-1]) {
-                pq.push({freq[nums[i]], nums[i]});
-                if (pq.size() > k) {
-                    pq.pop();
-                }
+        vector<vector<int>> bucket(n + 1);
+        for (auto x : freq) {
+            bucket[x.second].push_back(x.first);
+        }
+        vector<int> result;
+        for (int i = n; i >= 0; i--) {
+            if (bucket[i].size() == 0) {
+                continue;
+            }
+            while (bucket[i].size() > 0 && k > 0) {
+                result.push_back(bucket[i].back());
+                bucket[i].pop_back();
+                k--;
             }
         }
-        vector<int> ans;
-        while (!pq.empty()) {
-            auto x = pq.top();
-            pq.pop();
-            ans.push_back(x.second);
-        }
-        return ans;
+        return result;
     }
 };
