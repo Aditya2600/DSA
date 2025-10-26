5b992593d2c7e72a8d4753c9e2e83b00cb7df304
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-10-26 07:14:20 +0530

    Time: 299 ms (53.09%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3848-analyze-subscription-conversion/analyze-subscription-conversion.sql
@@ -0,0 +1,35 @@
+-- # Write your MySQL query statement below
+-- WITH trial_avg AS (
+--   SELECT 
+--     user_id, 
+--     ROUND(AVG(activity_duration), 2) AS trial_avg_duration
+--   FROM UserActivity
+--   WHERE activity_type = 'free_trial'
+--   GROUP BY user_id
+-- ),
+-- paid_avg AS (
+--   SELECT 
+--     user_id, 
+--     ROUND(AVG(activity_duration), 2) AS paid_avg_duration
+--   FROM UserActivity
+--   WHERE activity_type = 'paid'
+--   GROUP BY user_id
+-- )
+-- SELECT
+--   t.user_id,
+--   t.trial_avg_duration,
+--   p.paid_avg_duration
+-- FROM trial_avg t
+-- JOIN paid_avg p
+--   ON t.user_id = p.user_id
+-- ORDER BY t.user_id;
+
+SELECT
+    user_id,
+    ROUND(AVG(CASE WHEN activity_type = 'free_trial' THEN activity_duration ELSE NULL END), 2) AS trial_avg_duration,
+    ROUND(AVG(CASE WHEN activity_type = 'paid' THEN activity_duration ELSE NULL END), 2) AS paid_avg_duration
+    
+FROM UserActivity
+GROUP BY user_id
+HAVING AVG(CASE WHEN activity_type = 'paid' THEN activity_duration ELSE NULL END) IS NOT NULL
+ORDER BY user_id
