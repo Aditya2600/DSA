fdaba6d2e3d1d5fef08cb0208ce748c30bdb5849
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-10-27 13:01:54 +0530

    Time: 4 ms (76.61%) | Memory: 20.5 MB (9.12%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,14 +1,33 @@
+// class Solution {
+// public:
+//     int firstUniqChar(string s) {
+//         vector<int> freq(26, 0);
+//         int n = s.size();
+//         for(int i=0; i<n; i++){
+//             freq[s[i] - 'a']++;
+//         }
+//         for(int i=0; i<n; i++){
+//             if(freq[s[i] - 'a'] == 1) return i;
+//         }
+//         return -1;
+//     }
+// };
+
 class Solution {
 public:
     int firstUniqChar(string s) {
         vector<int> freq(26, 0);
         int n = s.size();
+        queue<int> q;
+
         for(int i=0; i<n; i++){
-            freq[s[i] - 'a']++;
+            int idx = s[i] - 'a';
+            freq[idx]++;
+            q.push(i);
+            while(!q.empty() && freq[s[q.front()] - 'a'] > 1){
+                q.pop();
+            }
         }
-        for(int i=0; i<n; i++){
-            if(freq[s[i] - 'a'] == 1) return i;
-        }
-        return -1;
+        return q.empty() ? -1 : q.front();
     }
 };
