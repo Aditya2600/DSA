3689d6b5a48c30c726da86b9a9cad854df89e09e
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-11-12 11:10:29 +0530

    Time: 0 ms (100.00%) | Memory: 30.3 MB (89.44%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,52 +1,47 @@
 class Solution {
 public:
-    
-    vector<vector<bool>> vis;
-    int bfs(int x, int y, vector<vector<int>>& grid){
-        queue<pair<int, int>> q;
+    void bfs(int row, int col, vector<vector<int>>& grid){
         int n = grid.size();
         int m = grid[0].size();
-        q.push({x, y});
-        vis[x][y] = true;
-        int dx[] = {1, 0, -1, 0};
-        int dy[] = {0, 1, 0, -1};
-        int walks = 1;
+        queue<pair<int, int>> q;
+        q.push({row, col});
+        grid[row][col] = 2;
+        int dr[] = {1, 0, -1, 0};
+        int dc[] = {0, 1, 0, -1};
+
         while(!q.empty()){
-            auto [r, c] = q.front();
+            int r = q.front().first;
+            int c = q.front().second;
             q.pop();
             for(int i=0; i<4; i++){
-                int nr = r + dx[i];
-                int nc = c + dy[i];               
-                if(nr < n && nr >= 0 && nc < m && nc >= 0 && grid[nr][nc] == 1 && !vis[nr][nc]){
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+
+                if(nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == 1){
                     q.push({nr, nc});
-                    vis[nr][nc] = true;
-                    walks++;
+                    grid[nr][nc] = 2;
                 }
-                
             }
         }
-        return walks;
     }
     int numEnclaves(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int ans = 0;
-        vis.resize(n, vector<bool> (m, false));
+        int land = 0;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(grid[i][j] == 1 && (i == 0 || i == n-1 || j == 0 || j == m-1) && !vis[i][j]){
-                    ans += bfs(i, j, grid);
+                if(grid[i][j] == 1 && (i == 0 || i == n-1 || j == 0 || j == m-1)){
+                    bfs(i, j, grid);
                 }
             }
         }
-        int cnt1 = 0;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(grid[i][j] == 1){
-                    cnt1++;
+                    land++;
                 }
             }
         }
-        return cnt1 - ans;
+        return land;
     }
 };
