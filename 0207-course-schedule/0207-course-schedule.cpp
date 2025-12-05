4a9e1cf07bcef2c25ca6fd59310122c8684b4ddb
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-12-05 09:47:39 +0530

    Time: 14 ms (12.92%), Space: 20 MB (43.84%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,47 +1,37 @@
 class Solution {
-private:
-    // DFS returns true if cycle is found
-    bool dfs(int node, vector<int>& vis, vector<int>& pathVis, vector<vector<int>>& adj) {
-        vis[node] = 1;
-        pathVis[node] = 1;          // node is in current recursion path
-
-        for (auto it : adj[node]) {
-            // FIX: yahan !vis[node] nahi, !vis[it] hoga
-            if (!vis[it]) {
-                if (dfs(it, vis, pathVis, adj)) return true;   // cycle in child
-            }
-            // if child is already in current path -> cycle
-            else if (pathVis[it]) {
-                return true;
-            }
-        }
-
-        pathVis[node] = 0;          // backtrack
-        return false;               // no cycle from this node
-    }
-
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
-        for (auto &it : prerequisites) {
+        for(auto it : prerequisites){
             int u = it[1];
             int v = it[0];
-            adj[u].push_back(v);    // u -> v
+            adj[u].push_back(v);
         }
-
-        vector<int> vis(numCourses, 0);
-        vector<int> pathVis(numCourses, 0);
-
-        // har component ke liye DFS chalao
-        for (int i = 0; i < numCourses; i++) {
-            if (!vis[i]) {
-                if (dfs(i, vis, pathVis, adj)) {
-                    // cycle mila -> saare courses finish nahi ho sakte
-                    return false;
+        int cnt = 0;
+        queue<int> qu;
+        vector<int> indegree(numCourses, 0);
+        for(int i=0; i<numCourses; i++){
+            for(auto it : adj[i]){
+                indegree[it]++;
+            }
+        }
+        
+        for(int i=0; i<numCourses; i++){
+            if(indegree[i] == 0){
+                qu.push(i);
+            }
+        }
+        while(!qu.empty()){
+            int node = qu.front();
+            qu.pop();
+            cnt++;
+            for(auto it : adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0){
+                    qu.push(it);
                 }
             }
         }
-        // no cycle -> all courses can be finished
-        return true;
+        return cnt == numCourses;
     }
 };
