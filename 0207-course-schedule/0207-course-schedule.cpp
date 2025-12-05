293f096a666de73e42c06b3f93f8dfc7427ec320
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-12-05 06:37:17 +0530

    Time: 3 ms (78.73%), Space: 19.4 MB (59.01%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,40 +1,47 @@
 class Solution {
+private:
+    // DFS returns true if cycle is found
+    bool dfs(int node, vector<int>& vis, vector<int>& pathVis, vector<vector<int>>& adj) {
+        vis[node] = 1;
+        pathVis[node] = 1;          // node is in current recursion path
+
+        for (auto it : adj[node]) {
+            // FIX: yahan !vis[node] nahi, !vis[it] hoga
+            if (!vis[it]) {
+                if (dfs(it, vis, pathVis, adj)) return true;   // cycle in child
+            }
+            // if child is already in current path -> cycle
+            else if (pathVis[it]) {
+                return true;
+            }
+        }
+
+        pathVis[node] = 0;          // backtrack
+        return false;               // no cycle from this node
+    }
+
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<list<int>> graph(numCourses);
-        for(auto it:prerequisites){
-            graph[it[1]].push_back(it[0]);
+        vector<vector<int>> adj(numCourses);
+        for (auto &it : prerequisites) {
+            int u = it[1];
+            int v = it[0];
+            adj[u].push_back(v);    // u -> v
         }
-        vector<int> indegree(numCourses,0);
-        for(int i=0; i<numCourses; i++){
-            for(auto neighbour : graph[i]){
-                indegree[neighbour]++;
-            }
-        }
-        queue<int> qu;
-        unordered_set<int> vis;
-        for(int i=0; i<numCourses; i++){
-            if(indegree[i] == 0){
-                qu.push(i);
-                vis.insert(i);
-            }
-        }
-        int count = 0;
-        while(not qu.empty()){
-            int node = qu.front();
-            qu.pop();
-            count++;
-            for(auto neighbour : graph[node]){
-                if(not vis.count(neighbour)){
-                    indegree[neighbour]--;
-                    if(indegree[neighbour] == 0){
-                        qu.push(neighbour);
-                        vis.insert(neighbour);
-                    }
+
+        vector<int> vis(numCourses, 0);
+        vector<int> pathVis(numCourses, 0);
+
+        // har component ke liye DFS chalao
+        for (int i = 0; i < numCourses; i++) {
+            if (!vis[i]) {
+                if (dfs(i, vis, pathVis, adj)) {
+                    // cycle mila -> saare courses finish nahi ho sakte
+                    return false;
                 }
             }
         }
-        if(count == numCourses) return true;
-        return false;
+        // no cycle -> all courses can be finished
+        return true;
     }
 };
