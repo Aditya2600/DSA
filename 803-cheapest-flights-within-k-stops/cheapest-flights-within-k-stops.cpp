7535750ab7346797840c2962a1266834fcc32c2a
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-12-07 22:56:26 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (42.62%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,45 +1,9 @@
-// class Solution {
-// public:
-//     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int
-//     dst, int k) {
-//         unordered_map<int, vector<pair<int, int>>> adj;
-//         for(auto it : flights){
-//             int u = it[0];
-//             int v = it[1];
-//             int w = it[2];
-//             adj[u].push_back({v, w});
-//         }
-
-//         vector<int> dist(n, 1e9);
-//         priority_queue<pair<int, int>, vector<pair<int, int>>,
-//         greater<pair<int, int>>> pq; pq.push({0, 0}); dist[0] = 0;
-//         while(!pq.empty()){
-//             int p = pq.top().first;
-//             int node = pq.top().second;
-//             pq.pop();
-//             k--;
-//             if(k == 0 && node == dst){
-//                 return p;
-//             }
-//             for(auto it : adj[node]){
-//                 int price = it.second;
-//                 int newNode = it.first;
-//                 if(p + price < dist[newNode]){
-//                     dist[newNode] = p + price;
-//                     pq.push({p + price, newNode});
-//                 }
-//             }
-//         }
-//         return -1;
-//     }
-// };
-
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst,
-                          int k) {
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int
+    dst, int k) {
         unordered_map<int, vector<pair<int, int>>> adj;
-        for (auto it : flights) {
+        for(auto it : flights){
             int u = it[0];
             int v = it[1];
             int w = it[2];
@@ -47,29 +11,65 @@ public:
         }
 
         vector<int> dist(n, 1e9);
-        queue<pair<int, int>> q;
-        q.push({0, src});
+        queue<pair<int,pair<int, int>>> q;
+        q.push({0, {src, 0}});
         dist[src] = 0;
-        int steps = 0;
-        while (!q.empty() && steps <= k) {
-            int N = q.size();
-            while (N--) {
-                int p = q.front().first;
-                int node = q.front().second;
-                q.pop();
-                for (auto it : adj[node]) {
-                    int price = it.second;
-                    int newNode = it.first;
-                    
-                    if (p + price < dist[newNode]) {
-                        dist[newNode] = p + price;
-                        q.push({p + price, newNode});
-                    }
+        while(!q.empty()){
+            int steps = q.front().first;
+            int node = q.front().second.first;
+            int price = q.front().second.second;
+            q.pop();
+            if(steps > k) continue;
+            for(auto it : adj[node]){
+                int newPrice = it.second;
+                int newNode = it.first;
+                if(newPrice + price < dist[newNode] && steps <= k){
+                    dist[newNode] = newPrice + price;
+                    q.push({steps+1,{newNode, newPrice + price}});
                 }
             }
-            steps++;
         }
         if(dist[dst] == 1e9) return -1;
         return dist[dst];
     }
 };
+
+// class Solution {
+// public:
+//     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst,
+//                           int k) {
+//         unordered_map<int, vector<pair<int, int>>> adj;
+//         for (auto it : flights) {
+//             int u = it[0];
+//             int v = it[1];
+//             int w = it[2];
+//             adj[u].push_back({v, w});
+//         }
+
+//         vector<int> dist(n, 1e9);
+//         queue<pair<int, int>> q;
+//         q.push({0, src});
+//         dist[src] = 0;
+//         int steps = 0;
+//         while (!q.empty() && steps <= k) {
+//             int N = q.size();
+//             while (N--) {
+//                 int p = q.front().first;
+//                 int node = q.front().second;
+//                 q.pop();
+//                 for (auto it : adj[node]) {
+//                     int price = it.second;
+//                     int newNode = it.first;
+                    
+//                     if (p + price < dist[newNode]) {
+//                         dist[newNode] = p + price;
+//                         q.push({p + price, newNode});
+//                     }
+//                 }
+//             }
+//             steps++;
+//         }
+//         if(dist[dst] == 1e9) return -1;
+//         return dist[dst];
+//     }
+// };
