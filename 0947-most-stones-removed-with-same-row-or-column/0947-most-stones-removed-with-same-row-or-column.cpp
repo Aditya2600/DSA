5ca0df48360391a89ae76703d3114b332fe80835
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-12-17 20:18:51 +0530

    Time: 47 ms (31.81%), Space: 18.3 MB (83.34%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,30 +1,85 @@
+// class Solution {
+// public:
+//     void dfs(vector<vector<int>>& stones, int index, vector<bool>& vis){
+//         vis[index] = true;
+//         for(int i=0; i<stones.size(); i++){
+//             int r = stones[index][0];
+//             int c = stones[index][1];
+
+//             if(vis[i] == false && (stones[i][0] == r || stones[i][1] == c)){
+//                 dfs(stones, i, vis);
+//             }
+//         }
+
+//     }
+//     int removeStones(vector<vector<int>>& stones) {
+//         int n = stones.size();
+
+//         vector<bool> vis(n, false);
+
+//         int group = 0;
+//         for(int i=0; i<n; i++){
+//             if(vis[i] == true){
+//                 continue;
+//             }
+//             dfs(stones, i, vis);
+//             group++;
+//         }
+//         return n - group;
+//     }
+// };
 class Solution {
 public:
-    void dfs(vector<vector<int>>& stones, int index, vector<bool>& vis){
-        vis[index] = true;
-        for(int i=0; i<stones.size(); i++){
-            int r = stones[index][0];
-            int c = stones[index][1];
+    int n;
+    vector<int> parent, rankv;
 
-            if(vis[i] == false && (stones[i][0] == r || stones[i][1] == c)){
-                dfs(stones, i, vis);
-            }
-        }
-
+    int findUPar(int x){
+        if(parent[x] == x) return x;
+        return parent[x] = findUPar(parent[x]);
     }
-    int removeStones(vector<vector<int>>& stones) {
-        int n = stones.size();
 
-        vector<bool> vis(n, false);
+    void Union(int u, int v){
+        int pu = findUPar(u);
+        int pv = findUPar(v);
+        if(pu == pv) return;
 
-        int group = 0;
-        for(int i=0; i<n; i++){
-            if(vis[i] == true){
-                continue;
-            }
-            dfs(stones, i, vis);
-            group++;
+        if(rankv[pu] < rankv[pv]){
+            parent[pu] = pv;
         }
-        return n - group;
+        else if(rankv[pu] > rankv[pv]){
+            parent[pv] = pu;
+        }
+        else{
+            parent[pv] = pu;
+            rankv[pu]++;
+        }
+    }
+
+    int removeStones(vector<vector<int>>& stones) {
+        n = stones.size();
+        parent.resize(n);
+        rankv.resize(n, 0);
+
+        for(int i = 0; i < n; i++){
+            parent[i] = i;
+        }
+
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                if(stones[i][0] == stones[j][0] ||
+                   stones[i][1] == stones[j][1]){
+                    Union(i, j);
+                }
+            }
+        }
+
+        int groups = 0;
+        for(int i = 0; i < n; i++){
+            if(findUPar(i) == i){
+                groups++;
+            }
+        }
+
+        return n - groups;
     }
 };
