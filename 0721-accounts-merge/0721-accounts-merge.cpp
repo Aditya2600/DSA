fe966c6cb6129a701fc3ec9e2f4a1a45ed42c54b
Aditya <126133127+Aditya2600@users.noreply.github.com>
2026-09-24 20:04:13 +0530

    Time: 27 ms (77.75%), Space: 40.4 MB (56.69%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -3,62 +3,62 @@ public:
     vector<int> parent;
     vector<int> size;
 
-    int findUPar(int x) {
-        if (x == parent[x]) {
-            return x;
+    int findUpar(int node) {
+        if (node == parent[node]) {
+            return node;
         }
-        return parent[x] = findUPar(parent[x]);
+        return parent[node] = findUpar(parent[node]);
     }
 
-    void unionBySize(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-
-        if (ulp_u == ulp_v) {
+    void unionBySize(int x, int y) {
+        int ulp_x = findUpar(x);
+        int ulp_y = findUpar(y);
+        if (ulp_x == ulp_y) {
             return;
         }
-
-        if (size[ulp_u] < size[ulp_v]) {
-            parent[ulp_u] = ulp_v;
-            size[ulp_v] += size[ulp_u];
+        if (size[x] > size[y]) {
+            parent[ulp_y] = ulp_x;
+            size[x] += size[y];
+        } else if (size[x] < size[y]) {
+            parent[ulp_x] = ulp_y;
+            size[y] += size[x];
         } else {
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v];
+            parent[ulp_x] = ulp_y;
+            size[y] += size[x];
         }
     }
-
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
         int n = accounts.size();
         parent.resize(n);
         size.resize(n, 1);
-        for (int i = 0; i < n; i++)
+        for(int i=0; i<n; i++){
             parent[i] = i;
-        unordered_map<string, int> mapMailNode;
-        for (int i = 0; i < n; i++) {
+        }
+        unordered_map<string, int> mp;
+        for (int i = 0; i < accounts.size(); i++) {
             for (int j = 1; j < accounts[i].size(); j++) {
-                string mail = accounts[i][j];
-                if (mapMailNode.find(mail) == mapMailNode.end()) {
-                    mapMailNode[mail] = i;
-                } else {
-                    unionBySize(i, mapMailNode[mail]);
+                if (mp.find(accounts[i][j]) == mp.end()){
+                    mp[accounts[i][j]] = i;
+                }
+                else {
+                    unionBySize(i, mp[accounts[i][j]]);
                 }
             }
         }
-        vector<vector<string>> mergedMail(n);
-        for (auto it : mapMailNode) {
+        vector<vector<string>> mergedAccounts(accounts.size());
+        for (auto& it : mp) {
             string mail = it.first;
-            int node = findUPar(it.second);
-            mergedMail[node].push_back(mail);
+            int node = findUpar(it.second);
+            mergedAccounts[node].push_back(mail);
         }
         vector<vector<string>> ans;
-
-        for (int i = 0; i < n; i++) {
-            if (mergedMail[i].size() == 0)
+        for (int i = 0; i < accounts.size(); i++) {
+            if (mergedAccounts[i].size() == 0)
                 continue;
-            sort(mergedMail[i].begin(), mergedMail[i].end());
+            sort(mergedAccounts[i].begin(), mergedAccounts[i].end());
             vector<string> temp;
             temp.push_back(accounts[i][0]);
-            for (auto it : mergedMail[i]) {
+            for (auto it : mergedAccounts[i]) {
                 temp.push_back(it);
             }
             ans.push_back(temp);
