76003f43898092a1af2d8fc257fbe19b9d865091
Aditya <126133127+Aditya2600@users.noreply.github.com>
2026-09-24 09:17:42 +0530

    Time: 15 ms (79.31%), Space: 47.9 MB (86.53%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -4,58 +4,62 @@ public:
         int key, val;
         node* prev;
         node* next;
-        node(int k, int v) : key(k), val(v), prev(nullptr), next(nullptr) {}
+        node(int k, int v): key(k), val(v), prev(nullptr), next(nullptr) {}
     };
     node* head = new node(-1, -1);
     node* tail = new node(-1, -1);
 
     int cap;
     unordered_map<int, node*> mp;
+    void deleteAtEnd(node* delNode){
+       node* prevNode = delNode->prev;
+       node* nextNode = delNode->next;
 
-    void deleteNode(node* delNode) {
-        node* prevNode = delNode->prev;
-        node* nextNode = delNode->next;
-        prevNode->next = nextNode;
-        nextNode->prev = prevNode;
+       prevNode->next = nextNode;
+       nextNode->prev = prevNode;
     }
-    void insertAfterHead(node* newNode) {
-        node* currAfterHead = head->next;
-        head->next = newNode;
-        newNode->next = currAfterHead;
+
+    void insertAtStart(node* newNode){
+        node* currentAfterHead = head->next;
         newNode->prev = head;
-        currAfterHead->prev = newNode;
+        newNode->next = currentAfterHead;
+        currentAfterHead->prev = newNode;
+        head->next = newNode;
+
     }
     LRUCache(int capacity) {
         cap = capacity;
         head->next = tail;
         tail->prev = head;
     }
-
+    
     int get(int key) {
-        if (mp.find(key) != mp.end()) {
+        if(mp.find(key) != mp.end()){
             node* Node = mp[key];
-            deleteNode(Node);
-            insertAfterHead(Node);
+            deleteAtEnd(Node);
+            insertAtStart(Node);
             return Node->val;
         }
         return -1;
     }
-
+    
     void put(int key, int value) {
-        if (mp.find(key) != mp.end()) {
+        if(mp.find(key) != mp.end()){
             node* Node = mp[key];
             Node->val = value;
-            deleteNode(Node);
-            insertAfterHead(Node);
-        } else {
-            if (mp.size() == cap) {
+            deleteAtEnd(Node);
+            insertAtStart(Node);
+        }
+        else{
+            if(mp.size() == cap){
                 node* Node = tail->prev;
+                deleteAtEnd(Node);
                 mp.erase(Node->key);
-                deleteNode(Node);
+                
             }
             node* Node = new node(key, value);
             mp[key] = Node;
-            insertAfterHead(Node);
+            insertAtStart(Node);          
         }
     }
 };
